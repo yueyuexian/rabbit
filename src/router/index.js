2da288d8f0728190956249b2c6ac0be858c5b38d
悦月 <3245995381@qq.com>
2025-05-30 13:55:29 +0800

    15. 二级分类整体认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import LoginVue from '@/view/Login/index.vue'
 import LayoutVue from '@/view/Layout/index.vue'
 import HomeVue from '@/view/Home/index.vue'
 import CategoryVue from '@/view/Category/index.vue'
+import SubCategoryVue from '@/view/SubCategory/index.vue'
 const routes = [
   {
     path: '/',
     component: LayoutVue,
     children: [
       { path: '', component: HomeVue },
-      { path: 'category/:id?', component: CategoryVue }
+      { path: 'category/:id?', component: CategoryVue },
+      { path: 'category/sub/:id?', component: SubCategoryVue }
     ]
   },
   { path: '/login', component: LoginVue }
