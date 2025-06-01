ed5c77f3a24151037c706b98a4fbcf5f364f0fde
悦月 <3245995381@qq.com>
2025-06-01 14:24:01 +0800

    21. 放大镜效果-滑块跟随鼠标移动

--- src/components/ImgView/index.vue
+++ src/components/ImgView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 // 图片列表
 const imageList = [
   'https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png',
@@ -13,6 +14,39 @@ const activeIndex = ref(0)
 const toggleBigImg = (index) => {
   activeIndex.value = index
 }
+// 2.获取鼠标相对位置
+const target = ref(null)
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+
+// 3.控制滑块跟随鼠标移动(监听elementX/Y变化，一旦变化 重新设置left/top值)
+const left = ref(0)
+const top = ref(0)
+watch([elementX, elementY], () => {
+  // 有效范围内控制滑块距离
+  // 横向
+  if (elementX.value > 100 && elementX.value < 300) {
+    left.value = elementX.value - 100
+  }
+  // 纵向
+  if (elementY.value > 100 && elementY.value < 300) {
+    top.value = elementY.value - 100
+  }
+
+  // 处理边界
+  if (elementX.value > 300) {
+    left.value = 200
+  }
+  if (elementX.value < 100) {
+    left.value = 0
+  }
+
+  if (elementY.value > 300) {
+    top.value = 200
+  }
+  if (elementY.value < 100) {
+    top.value = 0
+  }
+})
 </script>
 
 <template>
@@ -21,7 +55,7 @@ const toggleBigImg = (index) => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <!-- :class="{ active: i === activeIndex }" 动态类名控制 在tab切换类应用很常见 -->
