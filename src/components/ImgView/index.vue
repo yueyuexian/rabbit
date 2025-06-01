b3d7c01fa0c1613cca9452b31d76848833c14ee3
悦月 <3245995381@qq.com>
2025-06-01 14:35:00 +0800

    22. 放大镜效果-大图显示以及优化

--- src/components/ImgView/index.vue
+++ src/components/ImgView/index.vue
@@ -21,7 +21,12 @@ const { elementX, elementY, isOutside } = useMouseInElement(target)
 // 3.控制滑块跟随鼠标移动(监听elementX/Y变化，一旦变化 重新设置left/top值)
 const left = ref(0)
 const top = ref(0)
+
+const positionX = ref(0)
+const positionY = ref(0)
 watch([elementX, elementY], () => {
+  // 如果鼠标没有移入到盒子里面, 直接不执行后面的逻辑
+  if (isOutside.value) return
   // 有效范围内控制滑块距离
   // 横向
   if (elementX.value > 100 && elementX.value < 300) {
@@ -46,6 +51,10 @@ watch([elementX, elementY], () => {
   if (elementY.value < 100) {
     top.value = 0
   }
+
+  // 控制大图显示
+  positionX.value = -left.value * 2
+  positionY.value = -top.value * 2
 })
 </script>
 
@@ -55,7 +64,7 @@ watch([elementX, elementY], () => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <!-- :class="{ active: i === activeIndex }" 动态类名控制 在tab切换类应用很常见 -->
@@ -74,12 +83,12 @@ watch([elementX, elementY], () => {
       class="large"
       :style="[
         {
-          backgroundImage: `url(${imageList[0]})`,
-          backgroundPositionX: `0px`,
-          backgroundPositionY: `0px`
+          backgroundImage: `url(${imageList[activeIndex]})`,
+          backgroundPositionX: `${positionX}px`,
+          backgroundPositionY: `${positionY}px`
         }
       ]"
-      v-show="false"
+      v-show="!isOutside"
     ></div>
   </div>
 </template>
