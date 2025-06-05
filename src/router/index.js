f22fce810bc6e729a4637707a4e12eda5819af38
悦月 <3245995381@qq.com>
2025-06-05 12:22:27 +0800

    48. 支付结果的展示

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import LoginVue from '@/view/Login/index.vue'
 import LayoutVue from '@/view/Layout/index.vue'
 import HomeVue from '@/view/Home/index.vue'
@@ -8,6 +8,7 @@ import DetailVue from '@/view/Detail/index.vue'
 import CartList from '@/view/CartList/index.vue'
 import CheckoutVue from '@/view/Checkout/index.vue'
 import PayVue from '@/view/Pay/index.vue'
+import PayCallbackVue from '@/view/Pay/PayBack.vue.vue'
 const routes = [
   {
     path: '/',
@@ -20,14 +21,16 @@ const routes = [
       { path: 'detail/:id', component: DetailVue },
       { path: 'cartlist', component: CartList },
       { path: 'checkout', component: CheckoutVue },
-      { path: 'pay', component: PayVue }
+      { path: 'pay', component: PayVue },
+      { path: 'paycallback', component: PayCallbackVue }
     ]
   },
   { path: '/login', component: LoginVue }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  // history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes,
   // 路由滚动行为定制
   scrollBehavior() {
